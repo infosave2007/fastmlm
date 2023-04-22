94d8b9452c6acef41bbaf84ca9cb712a6afca327
infosave2007 <50740197+infosave2007@users.noreply.github.com>
2023-04-22 18:18:09 +0300

    Delete Documents/mlm directory

--- Documents/mlm/examples/delete_user.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once '../public/index.php';
-
-// Пример удаления пользователя
-$userId = 5; // Замените на ID пользователя, которого вы хотите удалить
-$userController->deleteUser($userId);
-echo "Deleted user with ID: {$userId}\n";
-?>
