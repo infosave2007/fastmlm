5deff1cdb25f39abbd2af4b55b6b11b1b7b4b7d1
infosave2007 <50740197+infosave2007@users.noreply.github.com>
2023-04-22 20:11:38 +0300

    Add files via upload

---
+++ database.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'host' => 'localhost',
+    'db_name' => 'your_database_name',
+    'username' => 'your_username',
+    'password' => 'your_password',
+    'charset' => 'utf8mb4',
+];
+
+?>
